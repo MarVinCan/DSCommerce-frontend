d9029225f2934d87de8ea40a264ca4d0df30f9e4
Marcus Vinicius <marcus.v.e.candido@gmail.com>
2024-02-08 13:25:31 -0300

    Searchbar onSearch event

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,12 +1,39 @@
-import './styles.css';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/ban-types */
+import { useState } from "react";
+import "./styles.css";
 
-export default function SearchBar(){
+type Props = {
+  onSearch: Function;
+};
 
-    return (
-        <form className="dsc-search-bar">
-            <button type="submit">🔎︎</button>
-            <input type="text" placeholder="Nome do produto" />
-            <button type="reset">🗙</button>
-          </form>
-    );
+export default function SearchBar({ onSearch }: Props) {
+  const [text, setText] = useState("");
+
+  function handleChange(event: any) {
+    setText(event.target.value);
+  }
+
+  function handleSubmit(event: any) {
+    event.preventDefault();
+    onSearch(text);
+  }
+
+  function handleResetClick(){
+    setText("");
+    onSearch(text);
+  }
+
+  return (
+    <form className="dsc-search-bar" onSubmit={handleSubmit}>
+      <button type="submit">🔎︎</button>
+      <input
+        value={text}
+        type="text"
+        placeholder="Nome do produto"
+        onChange={handleChange}
+      />
+      <button onClick={handleResetClick}>🗙</button>
+    </form>
+  );
 }
--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -24,7 +24,7 @@ export default function ProductDetails() {
       .catch(() => {
         navGate("/");
       });
-  }, []);
+  }, [navGate, params.productId]);
 
   function handleBuyClick() {
     if(product){
