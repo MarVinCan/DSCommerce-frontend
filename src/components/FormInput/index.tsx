f7b98ff5b3a14d83368335127e7f11dc2a6908ca
Marcus Vinicius <marcus.v.e.candido@gmail.com>
2024-02-14 14:32:59 -0300

    Custom form input

---
+++ src/components/FormInput/index.tsx
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+export default function FormInput(props: any) {
+  const { validation, ...inputProps } = props;
+
+  return <input {...inputProps} />;
+}
