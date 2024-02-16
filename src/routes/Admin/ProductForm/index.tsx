ade618c35a871f01f8f4eaa961215b86c65e1233
Marcus Vinicius <marcus.v.e.candido@gmail.com>
2024-02-16 20:05:33 -0300

    Update products with PUT

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import "./styles.css";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import FormInput from "../../../components/FormInput";
 import * as forms from "../../../utils/forms";
@@ -15,6 +15,8 @@ export default function ProductForm() {
   const params = useParams();
 
   const isEditing = params.productId !== "created";
+
+  const navigte = useNavigate();
 
   const [categories, setCategories] = useState<CategoryDTO[]>([]);
 
@@ -103,7 +105,16 @@ export default function ProductForm() {
       setFormData(formDataValidated);
       return;
     }
-    //console.log(forms.toValues(formData));
+    const requestBody = forms.toValues(formData);
+    if (isEditing){
+      requestBody.id = params.productId;
+    }
+
+    productService.updateRequest(requestBody)
+      .then(()=>{
+        navigte("/admin/products")
+      });
+
   }
   return (
     <main>
--- src/services/product-service.ts
+++ src/services/product-service.ts
@@ -1,5 +1,6 @@
 import { AxiosRequestConfig } from "axios";
 import { requestBackend } from "../utils/requests";
+import { ProductDTO } from "../models/product";
 
 export function findPageRequest(
   page: number,
@@ -31,5 +32,14 @@ export function deleteById(id: number){
     url: `/products/${id}`,
     withCredentials: true
   };
+  return requestBackend(config);
+}
+export function updateRequest(obj: ProductDTO){
+  const config: AxiosRequestConfig = {
+    method:"PUT",
+    url:`/products/${obj.id}`,
+    withCredentials: true,
+    data: obj
+  }
   return requestBackend(config);
 }
