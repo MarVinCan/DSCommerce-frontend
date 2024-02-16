c45b95590a6763625dad4970239bb311776d63e3
Marcus Vinicius <marcus.v.e.candido@gmail.com>
2024-02-16 14:35:13 -0300

    React Select, integration

---
+++ src/components/FormSelect/index.tsx
@@ -0,0 +1,29 @@
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import Select from "react-select";
+
+export default function FormSelect(props: any) {
+  const {
+    className,
+    validation,
+    invalid = "false",
+    dirty = "false",
+    onTurnDirty,
+    ...selectProps
+  } = props;
+
+  function handleBlur() {
+    onTurnDirty(props.name);
+  }
+
+  return (
+    <div
+    className={className} 
+    data-dirty={dirty} 
+    data-invalid={invalid}>
+      <Select {...selectProps} 
+      onBlur={handleBlur} />
+    </div>
+  );
+}
